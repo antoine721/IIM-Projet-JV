8e4df7aaa53b62136a8c019d92b972cf9edf760d
antoine721 <antoine.bbrachet@gmail.com>
2024-05-16 19:53:13 +0200

    pre-rendu
    beta 1.0

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce; // Nouvelle variable pour la force du saut
+
+    private bool isGrounded;
+    [HideInInspector]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public CapsuleCollider2D playerCollider;
+
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    public static PlayerMovement instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerMovement dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+
+        // Vérifiez si la touche espace est enfoncée et si le joueur est au sol
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump(); // Appel de la fonction de saut
+        }
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+
+        // Vérifiez si le personnage est au sol
+        if (isGrounded)
+        {
+            Debug.Log("Grounded");
+        }
+
+        MovePlayer(horizontalMovement, verticalMovement);
+    }
+
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+    void Jump() // Fonction de saut
+    {
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
